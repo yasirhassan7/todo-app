ed4d1d4f142dc8b6720608e5b02bba066adb6517
Yasir Hassan <yasirhassan@Hassans-MBP-79.lan>
2024-12-07 20:06:04 -0800

    Created a zip GUI

--- gui.py
+++ gui.py
@@ -2,9 +2,24 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label, input_box, add_button]])
-window.read() #displays window
+window = sg.Window('My To-Do App',
+                   layout=[[label, input_box, add_button]],
+                   font=('Helvetica', 20))
+
+
+while True:
+    event, values = window.read() #displays window
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] +"\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED: #closes the program when the red close button is pushed
+            break
+
+
 window.close()
